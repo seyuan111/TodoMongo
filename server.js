f708ccb3956ec2200ea29ce0f808c2728a6516ea
zakariasan <elitziko@gmail.com>
2022-08-06 10:39:58 +0100

    Add GET / POST and UI

--- server.js
+++ server.js
@@ -1,20 +1,54 @@
-const express = require('express');
+// -------------------------------------
+// TodoList using Mongo_db EJS nodeJS
+// --------------------------------------
+
+const express = require("express");
 const app = express();
-const MongoClient = require('mongodb').MongoClient
-const PORT = 8000;
-require('dotenv').config()
+const MongoClient = require("mongodb").MongoClient;
+const PORT = process.env.PORT || 1337;
 
+//=======================================
+// Link to Database -_"
+//=======================================
+
+require("dotenv").config();
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+	dbConnectionStr = process.env.DB_STRING,
+	dbName = "todoList";
 
-MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
-    .then(client => {
-        console.log(`We are connecting you to ${dbName}`)
-        db = client.db(dbName)
-    })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+	(client) => {
+		console.log(`We are connecting you to ${dbName}`);
+		db = client.db(dbName);
+	}
+);
 
-app.set('view engine', 'ejs')
-app.use(exxpress.static('public'))
-app.use(express.urlencoded({extended: true}))
-appp.use(express.json())
+//=======================================
+// Middlewares :
+//=======================================
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//======================================
+// Routes :
+//=======================================
+
+app.get("/", async (req, res) => {
+	const Items = await db.collection("todos").find().toArray();
+	const LeftTodo = await db
+		.collection("todos")
+		.countDocuments({ completed: false });
+	res.render("index.ejs", { items:Items, LeftTodo: LeftTodo });
+});
+
+app.post("/addTodo", async (req, res) => {
+	await db
+		.collection("todos")
+		.insertOne({ task: req.body.task, completed: false });
+	console.log("task added : =>", req.body.task);
+	res.redirect("/");
+});
+
+app.listen(PORT, () => console.log(`app on PORT :${PORT} `));
